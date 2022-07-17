1c1f95009775f1cf7cf72679aa1ec9336025c674
sannnok <30297191+sannnok@users.noreply.github.com>
2022-07-17 23:19:36 +0300

    set:define_blueprint_prepare-canvas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'super-duper-social-net-like'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('super-duper-social-net-like');
   });
 
   it('should render title', () => {
