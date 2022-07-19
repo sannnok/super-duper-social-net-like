b17467d1683c55913976c60d90bad33c0caafa0f
sannnok <30297191+sannnok@users.noreply.github.com>
2022-07-19 21:43:14 +0300

    feat:pointer_trailing_by_perlin_noise

--- src/app/directives/flow.directive.ts
+++ src/app/directives/flow.directive.ts
@@ -1,5 +1,14 @@
-import { Directive, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
-import { discardPeriodicTasks } from '@angular/core/testing';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { interval, timer } from 'rxjs';
+import { generatePerlinNoise } from '../utils/utils';
+
+interface Particular {
+  startPoint: {x: number, y: number},
+  noiseTrack: number[],
+  div: HTMLDivElement,
+  currentDrawIndex: number,
+  isSum: boolean;
+}
 
 @Directive({
   selector: '[appFlow]'
@@ -7,10 +16,24 @@ import { discardPeriodicTasks } from '@angular/core/testing';
 export class FlowDirective {
   flowing: boolean = false;
 
-  // Todo: Perlin noise flying things
+  mouse = {
+    x: 0,
+    y: 0
+  }
+
+  options = {
+    trailLength: 20,
+    size: 50,
+    interval: 30,
+  };
+
+  intervalMs    = this.options.interval || 15;
+  particulars: Particular[] = [];
+
   @HostListener('mousedown', ['$event']) public onMouseDown(event: MouseEvent) {
     this.flowing = true;
     this.defineEventSource(event);
+    console.log('click');
   }
   @HostListener('mousemove', ['$event']) public onMove(event: MouseEvent) {
     if (!this.flowing) return;
@@ -23,6 +46,8 @@ export class FlowDirective {
   @HostListener('touchstart', ['$event']) public onTouchStart(event: TouchEvent) {
     this.flowing = true;
     this.defineEventSource(event);
+    console.log('tap');
+
   }
   @HostListener('touchmove', ['$event']) public onTouchMove(event: TouchEvent) {
     if (!this.flowing) return;
@@ -33,11 +58,48 @@ export class FlowDirective {
     
   }
   
-  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  constructor(private renderer: Renderer2) {
+    this.initUpdates();
+  }
 
+  private setTheNoise() {
+    let noise = generatePerlinNoise(1, 1000, {
+      octaveCount: 10,
+      persistence: 0.2,
+      amplitude: 0.01
+    })
+
+    return noise.map(n => Number(n.toString().split('').splice(3, 2).join('')) * 1)
+  }
+
+  private initUpdates() {
+    interval(this.intervalMs).subscribe(() => this.updateTrackPositions())
+  }
+
+  private updateTrackPositions() {
+    for (var i = 0; i < this.particulars.length; i++) {
+      const curParticular = this.particulars[i];
+      const startPoint = curParticular.startPoint;
+      const noisefuncForThisPoint = curParticular.noiseTrack;
+
+      if (curParticular.currentDrawIndex == 0) {
+        curParticular.currentDrawIndex = Math.floor(Math.random() * curParticular.noiseTrack.length / 2);
+      }
+
+      const noiseX = curParticular.isSum
+        ? curParticular.startPoint.x + noisefuncForThisPoint[curParticular.currentDrawIndex]
+        : curParticular.startPoint.x - noisefuncForThisPoint[curParticular.currentDrawIndex];
+
+      startPoint.y -= 1;
+      const goingUpY = startPoint.y;
+
+      curParticular.div.style.top = goingUpY + 'px';
+      curParticular.div.style.left = noiseX + 'px';
+      curParticular.currentDrawIndex++;
+    }
+  }
 
   private drawFlowing(x: number, y: number) {
-    console.log(x, y, this.el)
     const div = this.renderer.createElement('div')
     const text = this.renderer.createText('❤️')
     this.renderer.appendChild(div, text)
@@ -46,16 +108,21 @@ export class FlowDirective {
     this.renderer.setStyle(div, 'top', y + 'px')
     this.renderer.setStyle(div, 'left', x + 'px')
     this.renderer.setStyle(div, 'transform', `translate(-50%, -50%)`)
-    // this.renderer.setStyle(div, 'transform', `translate3d(${x}px, ${y}px, 0px)`)
 
-    console.log(`translate3d(${x}px, ${y}px, 0px)`)
+    const particular: Particular = {
+      div,
+      noiseTrack: this.setTheNoise(),
+      startPoint: {x, y},
+      currentDrawIndex: 0,
+      isSum: !!Math.floor(Math.random() * 2)
+    }
 
-    setTimeout(() => this.renderer.removeChild(document.body, div), 500)
-    
+    this.particulars.push(particular);
 
-    // this.renderer.setStyle(this.divHello, 'height', '10px')
-    // this.renderer.setStyle(this.divHello, 'width', '10px')
-    // this.renderer.setStyle(this.divHello, 'background', 'black')
+    timer(10000).subscribe(() => {
+      this.particulars = this.particulars.filter(p => p.div !== div)
+      this.renderer.removeChild(document.body, div)
+    })
   }
 
   private defineEventSource(event: MouseEvent | TouchEvent) {
@@ -68,6 +135,6 @@ export class FlowDirective {
       y = event.touches[0].clientY;
     }
 
-    setTimeout(() => this.drawFlowing(x, y), 10)
+    this.drawFlowing(x, y)
   }
 }
--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -7,3 +7,71 @@ export const getLayers = (): Leaflet.Layer[] => {
     } as Leaflet.TileLayerOptions),
   ] as Leaflet.Layer[];
 }
+
+export function generatePerlinNoise(width: number, height: number, options?: { octaveCount?: any; amplitude?: any; persistence?: any; }) {
+  options = options || {};
+  var octaveCount = options.octaveCount || 4;
+  var amplitude = options.amplitude || 0.1;
+  var persistence = options.persistence || 0.2;
+  var whiteNoise = generateWhiteNoise(width, height);
+
+  var smoothNoiseList = new Array(octaveCount);
+  var i;
+  for (i = 0; i < octaveCount; ++i) {
+    smoothNoiseList[i] = generateSmoothNoise(i);
+  }
+  var perlinNoise = new Array(width * height);
+  var totalAmplitude = 0;
+  // blend noise together
+  for (i = octaveCount - 1; i >= 0; --i) {
+    amplitude *= persistence;
+    totalAmplitude += amplitude;
+
+    for (var j = 0; j < perlinNoise.length; ++j) {
+      perlinNoise[j] = perlinNoise[j] || 0;
+      perlinNoise[j] += smoothNoiseList[i][j] * amplitude;
+    }
+  }
+  // normalization
+  for (i = 0; i < perlinNoise.length; ++i) {
+      perlinNoise[i] /= totalAmplitude;
+  }
+
+  return perlinNoise;
+
+  function generateSmoothNoise(octave: number) {
+    var noise = new Array(width * height);
+    var samplePeriod = Math.pow(2, octave);
+    var sampleFrequency = 1 / samplePeriod;
+    var noiseIndex = 0;
+    for (var y = 0; y < height; ++y) {
+      var sampleY0 = Math.floor(y / samplePeriod) * samplePeriod;
+      var sampleY1 = (sampleY0 + samplePeriod) % height;
+      var vertBlend = (y - sampleY0) * sampleFrequency;
+      for (var x = 0; x < width; ++x) {
+        var sampleX0 = Math.floor(x / samplePeriod) * samplePeriod;
+        var sampleX1 = (sampleX0 + samplePeriod) % width;
+        var horizBlend = (x - sampleX0) * sampleFrequency;
+
+        // blend top two corners
+        var top = interpolate(whiteNoise[sampleY0 * width + sampleX0], whiteNoise[sampleY1 * width + sampleX0], vertBlend);
+        // blend bottom two corners
+        var bottom = interpolate(whiteNoise[sampleY0 * width + sampleX1], whiteNoise[sampleY1 * width + sampleX1], vertBlend);
+        // final blend
+        noise[noiseIndex] = interpolate(top, bottom, horizBlend);
+        noiseIndex += 1;
+      }
+    }
+    return noise;
+  }
+}
+function generateWhiteNoise(width: number, height: number) {
+  var noise = new Array(width * height);
+  for (var i = 0; i < noise.length; ++i) {
+    noise[i] = Math.random();
+  }
+  return noise;
+}
+function interpolate(x0: number, x1: number, alpha: number) {
+  return x0 * (1 - alpha) + alpha * x1;
+}
